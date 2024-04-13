e6976d28b24bcb1e80d97dbd0d74e9604ada580d
unreadying <147216529+unreadying@users.noreply.github.com>
2024-04-13 18:41:34 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ var hc = true;
 var cp = "h";
 var body;
 var grabbing = false;
-modecheck();
 
-document.getElementById("dm").onclick=dark;
-document.getElementById("hc").onclick=hcont;
+
+
 document.getElementById("lc").onclick=np;
 document.getElementById("rc").onclick=np;
 document.getElementById("c").onclick=np;
@@ -44,6 +43,7 @@ function np(event){
         document.querySelector('.contact').style.opacity = 0;
         document.querySelector('.ourwork').style.opacity = 0;
         document.querySelector('.about').style.opacity = 100;
+        document.querySelector('.aboutm').style.opacity = 100;
         document.querySelector('.nlc').style.opacity = .3;
         document.querySelector('.nrc').style.opacity = 3;
         document.querySelector('.nc').style.opacity = 3;
@@ -56,6 +56,7 @@ function np(event){
         cp="o"
         document.querySelector('.contact').style.opacity = 0;
         document.querySelector('.about').style.opacity = 0;
+        document.querySelector('.aboutm').style.opacity = 0;
         document.querySelector('.ourwork').style.opacity = 100;
         document.querySelector('.nc').style.opacity = .3;
         document.querySelector('.nrc').style.opacity = 3;
@@ -67,6 +68,7 @@ function np(event){
     if(event.target.innerHTML=="contact"){
         cp="c"
         document.querySelector('.about').style.opacity = 0;
+        document.querySelector('.aboutm').style.opacity = 0;
         document.querySelector('.ourwork').style.opacity = 0;
         document.querySelector('.contact').style.opacity = 100;
         document.querySelector('.nrc').style.opacity = .3;
@@ -121,89 +123,3 @@ function ddtog() {
   function arow(event){
     if(event.keyCode==27){ddtog();}
 }
-
-  function modecheck(){
-    if(localStorage.getItem("darkmode")=="true"){
-        dm=true;
-        document.querySelector("body").style.backgroundColor="hsla(0, 0%, 0%, 0.90)";
-        document.getElementById("dm").checked="checked";
-    }
-    if(localStorage.getItem("highcontrast")=="true"){
-        hc=true;
-        document.getElementById("hc").checked="checked";
-    }
-    hc = !hc;
-    hcont();
-    dm = !dm;
-    dark();
-}
-
-function dark(){
-    dm = !dm;
-    if(dm==true){
-    if(hc==true){
-    cchange("tc","white");
-    document.querySelector("body").style.backgroundColor="black";
-    }else {
-    cchange("tc","black");
-    document.querySelector("body").style.backgroundColor="hsla(0, 0%, 0%, 0.90)";
-    }
-    cchange("hctc","white");
-    document.getElementById("elip").style.color="white";
-    document.getElementById("hbtn").style.color="white";
-    console.log("dark mode engaged");
-    localStorage.setItem("darkmode","true");
-    }else if(dm==false){
-    if(hc==true){
-    cchange("tc","black");
-    document.querySelector("body").style.backgroundColor="white";
-    }else{
-    cchange("tc","white");
-    document.querySelector("body").style.backgroundColor="hsla(0, 0%, 95%, 1)";
-    }
-    cchange("hctc","black");
-    document.getElementById("elip").style.color="black";
-    document.getElementById("hbtn").style.color="black";
-    console.log("dark mode disengaged");
-    localStorage.setItem("darkmode","false");
-    }
-}
-
-function hcont(){
-    hc = !hc;
-    if(hc==true){
-    if(dm==false){
-    cchange("tc","black");
-    document.querySelector("body").style.backgroundColor="white";
-    }else if(dm==true){
-    cchange("tc","white");
-    document.querySelector("body").style.backgroundColor="black";
-    }    
-    console.log("contrast engaged");
-    localStorage.setItem("highcontrast","true");
-    }else if(hc==false){
-    if(dm==false){
-    cchange("tc","white");
-    document.querySelector("body").style.backgroundColor="hsla(0, 0%, 95%, 1)"
-    }else if(dm==true){
-    cchange("tc","black");
-    document.querySelector("body").style.backgroundColor="hsla(0, 0%, 0%, 0.90)";
-    }
-    console.log("contrast disengaged");
-    localStorage.setItem("highcontrast","false");
-}
-}
-
-function cchange(x,c){
-    elements = document.getElementsByClassName(x);
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].style.color=c;
-    }
-}
-
-function link(){
-    console.log(event);
-    if(event.target.innerHTML=="home"){document.location="../index.html"; return}
-    document.location="../"+event.target.innerHTML+"/"+event.target.innerHTML+".html";
-}
-
